d9d872b964a396a8b7501fa585432c0550bb6065
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-08-28 00:15:29 -0500

    transfer branch

--- www/js/index.js
+++ www/js/index.js
@@ -35,19 +35,9 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 
-        alert("Hola 4");
+        alert("Hola tu");
         //var URL = "http://thcoper.com/plantilla-parametrizada-diper.xls";
         var URL = "https://thcoper.com/acta-evaluacion-aptitud-psicofisica-final.pdf";
-        app.properties.ref =  cordova.InAppBrowser.open(URL, '_blank', 'location=yes');
-
-        var sync = ContentSync.sync({
-            src: URL,
-            id: 'movie-1'
-        });
-        
-        sync.on('progress', function(data) {
-            alert("descargando babe");
-        });
 
     },
     // Update DOM on a Received Event
